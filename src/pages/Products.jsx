e60e47cb7c4929538f4dfcd868b81e14c7693337
Olutomisin Oluwajuwon <jolutomisin@gmail.com>
2023-08-21 14:59:17 +0100

    finish products page

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,5 +1,7 @@
 import { styled } from "styled-components";
 import Newsletter from "../components/Newsletter";
+import { BiMinus } from 'react-icons/bi';
+import { AiOutlinePlus } from 'react-icons/ai';
 
 const ProductsSection = styled.section`
     
@@ -24,6 +26,7 @@ const InfoContainer = styled.div`
 `
 const Title = styled.h1`
     font-weight: 200;
+    font-size: 42px;
 `
 const Desc = styled.p`
     margin: 1.2rem 0;
@@ -33,10 +36,81 @@ const Price = styled.span`
     font-size: 2.5rem;
 `
 const FilterContainer = styled.div`
-    
+    width: 50%;
+    display: flex;
+    justify-content: space-between;
+    margin-top: 30px;
 
 `
+const FilterOne = styled.div`
+    span {
+        font-size: 20px;
+        font-weight: 200;
+    }
 
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+
+`
+const FilterColor = styled.div`
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    background-color: ${props => props.color};
+    cursor: pointer;
+`
+const FilterTwo = styled.div`
+    span {
+        font-size: 20px;
+        font-weight: 200;
+    }
+
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+`
+const FilterSizeSelect = styled.select`
+    cursor: pointer;
+    padding: 5px;
+`
+const SizeOption = styled.option`
+`
+const AddContainer = styled.div`
+    width: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-top: 30px;
+`
+const AmountContainer = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+    font-weight: 700;
+`
+const Amount = styled.span`
+    width: 30px;
+    height: 30px;
+    border-radius: 6px;
+    border: 1px solid teal;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+const Button = styled.button`
+    cursor: pointer;
+    text-transform: uppercase;
+    outline: none;
+    padding: 15px;
+    border: 2px solid teal;
+    background-color: white;
+    font-weight: 500;
+
+    &:hover {
+        background-color: #f8f4f4;
+    }
+`
 
 const Products = () => {
     return (  
@@ -50,7 +124,38 @@ const Products = () => {
                     <Desc>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit rem, aliquid mollitia, harum veniam quos perspiciatis quia, temporibus nisi deserunt asperiores commodi voluptatum et sequi aspernatur debitis! Quas, nisi dolor?
                     Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi, corporis reiciendis praesentium modi deserunt iure at, laborum quis molestiae perferendis odit magni explicabo eaque culpa consectetur sunt aliquam ad non!
                     </Desc>
-                    <Price>₦ 2,000</Price>
+                    <Price>₦ 4,500</Price>
+                    <FilterContainer>
+                        <FilterOne>
+                            <span>Color</span>
+
+                            <FilterColor color='black' />
+                            <FilterColor color='darkblue' />
+                            <FilterColor color='gray' />
+                        </FilterOne>
+
+                        <FilterTwo>
+                            <span>Size</span>
+
+                            <FilterSizeSelect>
+                                <SizeOption>XS</SizeOption>
+                                <SizeOption>S</SizeOption>
+                                <SizeOption>M</SizeOption>
+                                <SizeOption>L</SizeOption>
+                                <SizeOption>XL</SizeOption>
+                                <SizeOption>XXL</SizeOption>
+                            </FilterSizeSelect>
+                        </FilterTwo>   
+                    </FilterContainer>
+                    <AddContainer>
+                        <AmountContainer>
+                            <BiMinus />
+                            <Amount>1</Amount>
+                            <AiOutlinePlus />
+                        </AmountContainer>
+
+                        <Button>Add to cart</Button>
+                    </AddContainer>
                 </InfoContainer>
             </ProductsWrapper>
             <Newsletter />
